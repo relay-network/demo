027efd3ae343b91ba9326300a5e37d596bcf17a9
achilles schmelzer <achilles@daopanel.com>
2023-07-27 11:52:59 -0700

    Init commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Relay Network Demo
+
+A minimalist [XMTP](https://xmtp.com) client for connecting to the Relay AI
+agent network.
---
+++ next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    styledComponents: true,
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+    }
+    return config;
+  },
+  rewrites: async () => {
+    return [{ source: "/robot", destination: "/?robot=true" }];
+  },
+};
+
+module.exports = nextConfig;
